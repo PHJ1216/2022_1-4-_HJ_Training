fc0f51c67b0ef2a0db1310f3193cd896377a6048
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-01-31 01:12:39 +0900

    22_01_30
    행렬 덧셈 출력

--- C/22_01_30.c
+++ C/22_01_30.c
@@ -1,27 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#define size 10
+#define size 3
+#define size2 3
 int main(void) {
-	//버블 정렬 프로그램
+	// 행렬 A B를 각각 선언 & 새로운 행렬 C추가
+	int A[size][size2] = { {1,0,0},{0,1,0},{0,0,1} };
+	int B[size][size2] = { {1,0,0},{0,1,0},{0,0,1} };
+	int C[size][size2] = { 0, };
+	// 반복문 원소 덧셈
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size2;j++) {
+			C[i][j] = A[i][j] + B[i][j];
 
-	//배열및 변수 선언
-	int array[size] = { 80,60,30,70,90,20,10,40,50,100 };
-	int i,j,k,tmp=0;
-
-	//정렬 반복문
-	for (k = 0;k<size;k++) {
-		for (i = 0; i < size - 1;i++) {
-			if (array[i] > array[i + 1]) {
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp; 
-			}
 		}
 	}
-	//정렬된 배열 출력
-	printf("[ ");
-	for (j = 0; j < size; j++) {
-		printf("%d  ", array[j]);
+	// C 행렬 출력
+	for (int x = 0;x<size;x++) {
+		for (int y = 0;y<size2;y++) {
+			printf("%d ",C[x][y]);
+		}
+		printf("\n");
 	}
-	printf("]");
+
+	return 0;
 }
