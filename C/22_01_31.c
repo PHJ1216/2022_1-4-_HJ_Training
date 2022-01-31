c1a39d57851004bc6cc6e2ef3edb42bf3095bf9e
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-01-31 17:13:48 +0900

    22_01_31
    버블 정렬을 이용한 최대, 최소구하기

--- C/22_01_31.c
+++ C/22_01_31.c
@@ -1,16 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#define size 12
+#define size 5
 int main(void) {
-	//기초 배열 문제
+	 //배열 Min, Max 문제
 
-	//배열 선언
-	int arr[size] = {31,29,31,30,31,30,31,31,30,31,30,31};
+	//배열 및 변수 선언
+	int arr[size] = { 0, };
+	int tmp = 0;
 
-	//반복문 출력
-	for (int i = 0; i < size; i++) {
-		printf("%d월의 %d일까지 있습니다.\n",i+1,arr[i]);
+	//입력받기
+	for (int i = 0; i < 5; i++) {
+		printf("정수를 입력하세용 : ");
+		scanf("%d", &arr[i]);
+		printf("\n");
 	}
+	//출력하기
+	for (int x = 0;x<5;x++) {
+		for (int y = 0;y<4;y++) {
+			if (arr[y] > arr[y + 1]) {
+				tmp = arr[y];
+				arr[y] = arr[y+1];
+				arr[y + 1] = tmp;
+			}
 
+		}
+	}
+	printf("%d %d", arr[0], arr[4]);
 	return 0;
 }
