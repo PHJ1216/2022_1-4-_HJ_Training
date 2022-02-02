cdb90e5031683eeb030c3c667efdbe47af6a9490
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-02 17:57:22 +0900

    22_02_02
    함수로 MAX값 찾기

--- C/22_02_02.c
+++ C/22_02_02.c
@@ -1,21 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-//함수 이용 더하기
-int add(int x, int y) {
-	int result;
+// 함수를 이용해 MAX찾기
 
-	result = x + y;
-	return result;
+//입력 함수
+int get_int() {
+	int val;
+	printf("정수 입력 : ");
+	scanf("%d", &val);
+	return val;
 }
-
+//if문 MAX찾기
+int find_max(int x,int y) {
+	int MAX;
+	if (x > y) {
+		MAX=x;
+	}
+	else if (x < y) {
+		 MAX=y;
+	}
+	return MAX;
+}
 int main(void){
-
 	int a, b;
+	a = get_int();
+	b = get_int();
+
 	int r;
-	printf("두 정수 입력 : ");
-	scanf("%d  %d", &a, & b);
-	r=add(a, b); //덧셈 함수 받기
-	printf("%d", r);
+	r = find_max(a,b);
+	printf("MAX값은 %d", r);
 
 
 	return 0;
