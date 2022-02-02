87ae531ae2f4c768c234348395e6442962884bfd
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-02 23:37:40 +0900

    22_02_02
    for문 함수 정의

--- C/22_02_02.c
+++ C/22_02_02.c
@@ -1,24 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+int sum = 0;
+// for 반복문 함수로 작성
 
-//함수 원형 선언
-double f_to_c(double temp_f);
+// 함수 원형 선언
+int for_(int x, int y);
 
 int main(void){
-	int f;
-	int c;
-	printf("화씨온도를 입력하세요 : ");
-	scanf("%d", &f);
 
-	c = f_to_c(f);
-	printf("섭씨온도 : %d", c);
-
+	int i, j;
+	printf("for문 시작 값 입력 : ");  
+	scanf("%d", &i);
+	printf("\n");
+	printf("for문 끝 값 입력 : "); //*n까지 계산할 시 n+1 입력*
+	scanf("%d", &j);
+	printf("\n");
+	printf("%d~%d 덧셈 값 : %d",i,(j-1), for_(i,j));
 	return 0;
 }
-//함수 선언
-double f_to_c(double temp_f) {
-	double change;
-	change = (5.0 * (temp_f- 32.0)) / 9.0;
+// for문 함수 정의
+int for_(int x, int y) {
+	
 
-	return change;
+	if (x == y) {
+		return sum;
+	}
+	else {
+		sum = sum + x;
+		x++;
+		return for_(x, y);
+	}
+
+	return;
 }
