6e6b8f13e576fb760948034c55b1a5d0b0e13e43
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-03 12:57:49 +0900

    22_02_03
    소수 검별 함수

--- C/22_02_03.c
+++ C/22_02_03.c
@@ -1,29 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-// 실수 대수 비교 함수
 
-double get_bs();
+int primeFC(int n);  //함수 선언문
 
-int main(void) {
 
-	double i, j;
-	printf("실수를 입력하세요 : ");
-	scanf("%lf", &i);
-	printf("실수를 입력하세요 : ");
-	scanf("%lf", &j);
-	printf("더 큰 수는 %lf입니다,",get_bs(i, j));
-
-	return 0;
+int main(void)
+{
+    int i = 0;
+    for (i = 1; i <= 100; i++) //i를 1부터 100까지 1씩 증가시키며 반복 수행
+    {
+        if (primeFC(i)) //i가 소수라면
+        {
+            printf("%3d ", i); //i를 출력
+        }
+    }
+    printf("\n");
+    return 0;
 }
 
-double get_bs(double x, double y) {
-	double result;
-	if (x > y) {
-		result = x;
-			return result;
-	}
-	else {
-		result = y;
-		return result;
-	}
+//함수명: primeFC
+//입력 인자로 전달받은 n이 소수인지 확인하는 함수
+
+int primeFC(int n) //함수 정의문
+{
+    int i = 0;
+    int last = n / 2;   //약수가 없는 수가 소수이므로 2부터 n/2까지만 확인하면 됨
+    if (n <= 1)//소수는 1보다 큰 자연수여야 함
+    {
+        return 0;//반환값0: 소수가 아님
+    }
+    //(자기자신/2)보다 큰수는 약수가 될 수 없음
+    for (i = 2; i <= last; i++) //i를 2부터 last까지 1씩 증가시키며 반복 수행
+    {
+        if ((n % i) == 0) //n을 i로 나누었을때 나머지가 0이면 소수 아님
+        {
+            return 0; //반환값0: 소수가 아님
+        }
+    }
+    return 1; //1~last사이에 약수가 없음 반환값1: 소수임
 }
