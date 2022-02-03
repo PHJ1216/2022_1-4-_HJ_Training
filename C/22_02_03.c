2a4c7a0e386cdc616d6eaef15e4c10be5b00036a
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-03 13:41:07 +0900

    22_02_03
    거듭제곱 계산 함수

--- C/22_02_03.c
+++ C/22_02_03.c
@@ -1,41 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+// 거급제곱 계산 함수
 
-int primeFC(int n);  //함수 선언문
-
-
+int impower(int n, int k); //함수 선언
+int b;
+int sum = 1;
 int main(void)
 {
-    int i = 0;
-    for (i = 1; i <= 100; i++) //i를 1부터 100까지 1씩 증가시키며 반복 수행
-    {
-        if (primeFC(i)) //i가 소수라면
-        {
-            printf("%3d ", i); //i를 출력
-        }
+    int a;
+    printf("정수 n입력 및 거듭 수 k 입력 : ");
+    scanf("%d %d", &a, &b);
+    for (int j = 0; j <= b; j++) {
+        printf("%d \n", impower(a,j));
     }
-    printf("\n");
     return 0;
 }
 
-//함수명: primeFC
-//입력 인자로 전달받은 n이 소수인지 확인하는 함수
-
-int primeFC(int n) //함수 정의문
+int impower(int n,int k) //함수 정의문
 {
-    int i = 0;
-    int last = n / 2;   //약수가 없는 수가 소수이므로 2부터 n/2까지만 확인하면 됨
-    if (n <= 1)//소수는 1보다 큰 자연수여야 함
-    {
-        return 0;//반환값0: 소수가 아님
-    }
-    //(자기자신/2)보다 큰수는 약수가 될 수 없음
-    for (i = 2; i <= last; i++) //i를 2부터 last까지 1씩 증가시키며 반복 수행
-    {
-        if ((n % i) == 0) //n을 i로 나누었을때 나머지가 0이면 소수 아님
-        {
-            return 0; //반환값0: 소수가 아님
+        if ( k == 0)  { // k가 0이면 반환값 1
+            sum = 1;
+            return sum;
         }
-    }
-    return 1; //1~last사이에 약수가 없음 반환값1: 소수임
+        else {          // k가 0이상인 숫자면 거급 제곱
+            sum = sum * n;
+            return sum;
+        }
+  
 }
