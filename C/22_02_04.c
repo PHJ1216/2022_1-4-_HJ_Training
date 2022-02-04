6187039fa67d3556ed128f0b6e568a0261dcfcf6
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-04 22:45:02 +0900

    22_02_04
    배열 순서 받은 뒤 버블 정렬 포인터 함수

--- C/22_02_04.c
+++ C/22_02_04.c
@@ -1,27 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
-#define SIZE 10
+#define SIZE 5
+// 배열 역순으로 출력 포인터 함수
 
-// 1차원 배열 합 계산 함수
-int get_arr_sum(int* A, int size); //함수 선언
+int reverse(int* p,int size); // 함수 선언
 
 int main(void)
 {
-	int data[SIZE] = { 1,2,3,4,5,6,7,8,9,0 };
-	
-	printf("배열 요소의 합 : %d",get_arr_sum(data, SIZE)); //*A=data
-	return 0;
-
-}
-
-int get_arr_sum(int *A, int size) //함수 정의
-{
-	int sum = 0;
-	
-	for (int j = 0; j < size; j++) {
-		sum =sum+ A[j];
+	int arr[SIZE] = { 0, };
+	printf("정수 5개를 입력 \n");
+	for (int dd = 0; dd < 5; dd++) {
+		printf("%d번째 정수 : ", dd + 1);
+		scanf("%d", &arr[dd]);
+		printf("\n");
 	}
-	return sum;
+	printf("[  ");
+	reverse(arr, SIZE);
+	printf(" ]");
+	return 0;
 }
 
+int reverse(int* p,int size) // 함수 정의
+{
+	int tmp = 0;
+	for (int j = 0; j < size; j++) {
+		for (int k = 0; k < size; k++) {
+			if (p[k] < p[k + 1]) {
+				tmp = p[k + 1];
+				p[k + 1] = p[k];
+				p[k] = tmp;
+			}
+		}//2for
+	}//1for
+	for (int num = 0; num < size; num++)
+	{
+		printf("%d  ", p[num]);
+	}
+}
