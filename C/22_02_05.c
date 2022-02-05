68c47cc2e293a325bd81c3592d0d89601ebf1a42
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-05 15:55:52 +0900

    22_02_05
    정수부 실수부 나누기 포인터 함수

--- C/22_02_05.c
+++ C/22_02_05.c
@@ -2,28 +2,23 @@
 #include<stdio.h>
 #include<math.h>
 #define SIZE 5
-// 2차 방정식 근 구하기 포인터 함수
+// 실수를 정수부 소수부로 나누기 포인터 함수
 
-int fc_2cha(int a, int b, int c, double* rms_m, double* rms_p); // 함수 선언
-
-
+void get(double value, int* i_part, double* f_part); //함수 선언
 int main(void)
 {
-	int x, y, z;
-	double rmsm;
-	double rmsp;
-	printf("2차항 계수 a / 1차항 계수 b / 상수항 c 입력 : ");
-	scanf("%d %d %d", &x, &y, &z);
-	printf("\n");
-	fc_2cha(x, y, z, &rmsm, &rmsp);
-	printf("%lf\n", rmsm);
-	printf("%lf\n", rmsp);
+	double abc = 3.14;
+	int ipt;
+	double  fpt;
+	get(abc, &ipt, &fpt);
+	printf("정수부 %d \n", ipt);
+	printf("실수부 %lf",fpt );
+
 	return 0;
 }
 
-int fc_2cha(int a, int b, int c, double* rms_m, double* rms_p) // 함수 정의
+void get(double value, int* i_part, double* f_part) //함수 정의 
 {
-
-	*rms_m = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-	*rms_p = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+	*i_part = value / 1;
+	*f_part = value - 3;
 }
