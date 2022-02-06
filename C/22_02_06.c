be6b9774b83df9154ca2410022d65ea9dc337815
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-06 12:52:15 +0900

    단어 애나그램 게임 프로그램

--- C/22_02_06.c
+++ C/22_02_06.c
@@ -3,29 +3,32 @@
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
-
-// 지정된 문자열 맞추기(행맨게임)
+#define num "apple"
+// 단어 애나그램 게임 프로그램 
 
 int main(void) {
 
-	char solution[] = "happy new year"; 
-	char answer[] = "_____ ___ ____";
-	char ch;
-	
-	while (1) {
-			printf("현재 : %s\n", answer);
-			printf("문자 입력 : ");
-			ch = getch();
-		
-		for (int i = 0; solution[i] != NULL; i++) // i가 0부터 solution배열의 NULL을 찾을 때 까지
-		{
-			
-			if (solution[i] == ch) { //solution이 입력한 문자와 같은 것을 발견하면
-				answer[i] = ch;  // answer도 같은 자리에 문자를 대입하겠다
-			}
-		}
+	char aenagram[100] = num;
+	char answer[100] = num;
+	char guess[100] ;
+	int len = strlen(num);
+	char tmp;
+	for (int i = 0; i < len; i++) {
+		int p1, p2;
+		p1 = rand() % len;
+		p2 = rand() % len;
 
-		if (strcmp(solution,answer)==0) { //똑같은 문자열이 되면 break
+		tmp = aenagram[p1];
+		aenagram[p1] = aenagram[p2];
+		aenagram[p2] = tmp;
+	}
+	while (1) {
+		printf("%s 무슨 단어인지 맞추세요.\n",aenagram);
+		printf("예측 단어 : ");
+		scanf("%s", guess);
+	
+		if (strcmp(guess,answer) == 0) {
+			printf("정답!");
 			break;
 		}
 	}
