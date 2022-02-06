0a2c2412b81bfa8de110e3f5de0cc4ce30e2abdd
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-06 13:30:43 +0900

    간이 한영사전 구현 프로그램

--- C/22_02_06.c
+++ C/22_02_06.c
@@ -4,34 +4,28 @@
 #include<string.h>
 #include<stdlib.h>
 #define num "apple"
-// 단어 애나그램 게임 프로그램 
+// 간이 한영사전 구현 프로그램
 
 int main(void) {
 
-	char aenagram[100] = num;
-	char answer[100] = num;
-	char guess[100] ;
-	int len = strlen(num);
-	char tmp;
-	for (int i = 0; i < len; i++) {
-		int p1, p2;
-		p1 = rand() % len;
-		p2 = rand() % len;
-
-		tmp = aenagram[p1];
-		aenagram[p1] = aenagram[p2];
-		aenagram[p2] = tmp;
-	}
+	char dic[5][2][30] = {// dic[i][j][k] i는 행 j는 행에서 문자열 2개 k는 해당 행에 대한 메모리크기 
+		{"computer","컴퓨터"},
+		{"c programming","c언어"},
+		{"java","자바"},
+		{"phython","파이썬"},
+		{"hyeonjun","현준"}
+	};
+	char ans[1][30];
 	while (1) {
-		printf("%s 무슨 단어인지 맞추세요.\n",aenagram);
-		printf("예측 단어 : ");
-		scanf("%s", guess);
-	
-		if (strcmp(guess,answer) == 0) {
-			printf("정답!");
-			break;
+		printf("한글을 입력하세요");
+		scanf("%s", ans);
+		for (int i = 0; i < 5; i++) {
+			if (strcmp(dic[i][1],ans)==0) {
+				printf("%s를 영어로 하면 %s입니다.\n",ans[0], dic[i][0]);
+				return 0;
+			}
+			
 		}
 	}
-
 	return 0;
 }
