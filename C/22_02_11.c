32d88df4453867d9baef3a0bd24d1f91dc6aeb31
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-11 11:18:33 +0900

    동적 배열을 난수로 채우는 프로그램

--- C/22_02_11.c
+++ C/22_02_11.c
@@ -4,30 +4,25 @@
 #include<string.h>
 #include<stdlib.h>
 
-// 동적 메모리 사용자가 입력하는 크기의 배열 만들기
+// 동적 배열을 난수로 채우는 프로그램
 
 int main(void) {
+	int* dm;
+	dm = (int*)malloc(1000 * sizeof(int));
+	for (int i = 0; i < 1000; i++) {
+		dm[i] = rand(); // 정수형 범위에서 난수 발생
+	}
 
-    char* pc;
-    int num;
-    printf("항목의 개수는 몇 개? : ");
-    scanf("%d", &num);
-    pc = (char*)malloc(num * sizeof(int)); //num의 크기만큼 배열 생성
-    if (pc == NULL) {
-        printf("메모리 할당 실패");
-        exit(1);
-    }
-    for (int i = 0; i < num; i++) {
-        printf("항목<정수> 입력 : ");
-        scanf("%d", &pc[i]);  //*pc=1,*(p+1)=2를 배열로 나타냄 각각 pc[0]=1,pc[1]=2
-        printf("\n");
-    }
-    printf("\n");
-    printf("입력된 값은 다음과 같습니다.\n");
-    for (int j = 0; j < num; j++) {
-        printf("%d  ",pc[j]);
-    }
-    free(pc);
-
-    return 0;
+	for (int x = 0;x<=1000;x++) {
+		for (int y = 0;y<1000;y++) {
+			if (dm[y] > dm[y + 1]) { // 버블 정렬을 이용한 최댓값 구하기
+				int tmp = dm[y];
+				dm[y] = dm[y + 1];
+				dm[y + 1] = tmp;
+			}
+		}
+	}
+	printf("MAX : %d", dm[999]);
+	free(dm);
+	return 0;
 }
