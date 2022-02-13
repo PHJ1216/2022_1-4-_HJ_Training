761f5201a7b12a8b0679acbd24e93de4f912b865
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-13 22:35:46 +0900

    정수 입력 하고 출력하기

--- Java/java22_02_13.java
+++ Java/java22_02_13.java
@@ -1,9 +1,16 @@
 
+import java.util.Scanner; //Scanner클래스를 사용하기 위해 추가
+
 public class java22_02_13 {
 
 	public static void main(String[] args) {
+		Scanner scr=new Scanner(System.in);
 		
-		System.out.println("hello, world!");
-	}
+		System.out.println("정수 입력 : ");
+		int num=scr.nextInt(); //정수형 num에 입력 내용을 저장
+		
+		System.out.println("정수 : "+num);
+		
 
+	}
 }
