32ac242660537ef0875780b980b2405941f15217
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-14 01:15:45 +0900

    형변환 사용 화씨를 섭씨로 변환코드

---
+++ Java/java22_02_14.java
@@ -0,0 +1,11 @@
+public class java22_02_14 {
+
+	public static void main(String[] args) {
+		
+
+		int f=100;
+		float c = (int)((5/9f*(f-32)*100+0.5))/100f;
+		System.out.println("È­¾¾ ¿Âµµ : "+f);
+		System.out.println("¼·¾¾ ¿Âµµ = "+c);
+	}
+}
