1e6a0d533edc0c3a9b148d591d3eb83eb8c955d1
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-14 14:46:07 +0900

    java if문 기초 문법 중첩 if문

--- Java/java22_02_14.java
+++ Java/java22_02_14.java
@@ -1,11 +1,40 @@
-public class java22_02_14 {
+import java.util.Scanner;
 
+public class java22_02_14 {
+	// java if문 기초 문법 중첩 if문
 	public static void main(String[] args) {
 		
-
-		int f=100;
-		float c = (int)((5/9f*(f-32)*100+0.5))/100f;
-		System.out.println("화씨 온도 : "+f);
-		System.out.println("섭씨 온도 = "+c);
-	}
-}
+		System.out.print("점수를 입력하세요 : ");
+		Scanner scr =new Scanner(System.in);
+		int num = scr.nextInt();
+		
+		char grade=' ', ch='0';
+		
+		if(num>90) {	
+			grade='A';
+			if(num>=98) {    //90점 이상이고 98점이상이면 A+
+				ch='+';
+			}
+			else if(num<=94) {  //90점 이상이지만 94점 이하면 A-
+				ch='-';
+			}}
+		
+		else if(num>80) {		//위 if문과 같은 알고리즘
+			grade='B';
+			if(num>=88) {
+				ch='+';
+			}
+			
+			else if(num<=84) {		
+				ch='-'; 
+			}
+			}
+		else {
+			grade='C';   //나머지는 C로 처리
+		}
+		System.out.printf("학점 : %c%c",grade,ch);
+		}
+		
+			}	
+		
+	
