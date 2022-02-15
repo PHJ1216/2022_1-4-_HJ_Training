341e31ee1c768f4ee08f182b619d3177e21000e6
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-15 17:40:49 +0900

    java while문으로 숫자열 합 구하기

--- Java/java22_02_15.java
+++ Java/java22_02_15.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 
 public class java22_02_15 {
-		//java switch문 문법
+		//java while문으로 숫자열 합 구하기
 	public static void main(String[] args) {
-		Scanner scr=new Scanner(System.in);
+		Scanner scr = new Scanner(System.in);
+		System.out.printf("정수 입력: ");
 		int num=scr.nextInt();
-		System.out.println(num);
-		
-		switch (num) {
-		case 3: case 4: case 5: 
-			System.out.println("봄~"); 
-			break;
-		case 6: case 7: case 8: 
-			System.out.println("여름~"); 
-			break;
-		case 9: case 10: case 11: 
-			System.out.println("가을~"); 
-			break;
-			
-		default:System.out.println("겨울~");
-			
+		int sum=0;
+		while(num!=0) {
+		sum=sum+num%10 ; //12345%10 = 5
+		System.out.println("num="+num+"      "+"sum"+sum);
+		num=num/10;
 		}
+		
+			
 		
 	}
 
