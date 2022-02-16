c916093aacf2fb5eb39695d21019f225b71f104c
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-16 22:34:05 +0900

    java 배열 기초 문법 - 배열 평균 구하기

--- Java/java22_02_16.java
+++ Java/java22_02_16.java
@@ -3,15 +3,14 @@ import java.util.Arrays;
 public class java22_02_16 {
 	// java 배열 기초 문법
 	public static void main(String[] args) {
-
-		int arr[] = new int[5] ; //배열 선언부
-		for (int i = 0; i < 5; i++) {
-			arr[i] = i;
-			System.out.println(arr[i]);
-
+			// 배열 평균 구하기
+		int[] arr={100,90,80,70,60};
+		int sum=0;
+		for(int i=0;i<5;i++) {
+			sum+=arr[i];
 		}
-		System.out.println(Arrays.toString(arr)); //배열 보기좋게 출력
-		System.out.println(arr); //배열이 가리키는 주소
+		System.out.println(sum/(float)arr.length); // float로 실수형 형변환
+		
 	} // main
 
 } // class
