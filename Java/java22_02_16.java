138230a37d4bb69efa5b13400c1ba839cb71f2c5
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-16 22:25:13 +0900

    java 배열 기초 문법

--- Java/java22_02_16.java
+++ Java/java22_02_16.java
@@ -1,21 +1,17 @@
+import java.util.Arrays;
 
 public class java22_02_16 {
-	// java 1+(-2)+3(-4)+...(+n)총합 100이 될떄 마지막 정수 구하기
+	// java 배열 기초 문법
 	public static void main(String[] args) {
 
-		int sum = 0; // 총합을 저장할 변수
-		int s = 1; // 값의 부호를 바꿔주는데 사용할 변수
-		int num = 0;
-		// 조건식의 값이 true이므로 무한반복문이 된다.
-		for (int i = 1; true; i++, s = -s) { // 매 반복마다 s의 값은 1, -1, 1, -1...  
-			num = s * i;// i와 부호(s)를 곱해서 더할 값을 구한다.
-			sum += num;
-			if (sum >= 100) // 총합이 100보다 같거나 크면 반복문을 빠져 나간다.
-				break;
-		}
-		System.out.println("num=" + num);
-		System.out.println("sum=" + sum);
+		int arr[] = new int[5] ; //배열 선언부
+		for (int i = 0; i < 5; i++) {
+			arr[i] = i;
+			System.out.println(arr[i]);
 
-	} // object	
+		}
+		System.out.println(Arrays.toString(arr)); //배열 보기좋게 출력
+		System.out.println(arr); //배열이 가리키는 주소
+	} // main
 
 } // class
