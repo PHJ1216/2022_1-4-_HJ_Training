c67a18b931cb5797ee2988d61cc185b88a439cc0
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-16 15:49:28 +0900

    java 1+(1+2)+(1+2+3)+...+(1+2+3+...+10) 구하기

--- Java/java22_02_16.java
+++ Java/java22_02_16.java
@@ -1,18 +1,14 @@
 
 public class java22_02_16 {
-	// java 2 또는 3의 배수가 아닌 수의 총합 구하기
+	// java 1+(1+2)+(1+2+3)+...+(1+2+3+...+10) 구하기
 	public static void main(String[] args) {
 
-		int i;
 		int sum = 0;
-		for (i = 1; i <= 21; i++) {
-			if (i % 2 == 0 || i % 3 == 0) {
-				continue;
-			}
-
-			sum += i;
+		for (int i = 1; i <= 10; i++) { // 수정시 범위는 <=n 까지
+			sum+=i*(11-i);  //만약 마지막이 n이라면 11자리에 n+1을 대입하면 된다
 			System.out.println(sum);
 		}
+		
 
 	} // object
 
