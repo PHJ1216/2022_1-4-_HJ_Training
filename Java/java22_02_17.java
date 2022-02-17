469a11627ec139bb51d5e2899661a5a84c3a7761
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-17 23:09:06 +0900

    java 배열을 이용한 단어 맞추기 프로그램

--- Java/java22_02_17.java
+++ Java/java22_02_17.java
@@ -1,28 +1,26 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class java22_02_17 {
 	// java 배열 기초 문법
 	public static void main(String[] args) {
-			// String 배열 문법
-
-		String[] name = new String[] {"park","yi","kim"};
-		System.out.println(Arrays.toString(name));
-		
-			//2차원 배열
-		
-		int[][] str= {
-				{1,2,3},
-				{4,5,6},
-				{7,8,9}
+		// 단어 맞추기 프로그램
+		Scanner scn=new Scanner(System.in);
+		String word[][] = {
+				{"computer","컴퓨터"}, //00 01
+				{"chair","의자"},	     //10 11
+				{"student","학생"}    //20 21
 		};
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				System.out.println("str["+i+"]["+j+"]="+str[i][j]); //2중 for문으로 출력 
-			}
+		for(int i=0;i<word.length;i++) {
+			System.out.println("Q"+(i+1)+": "+word[i][0]+"의 답은?");
+			String str=scn.nextLine();
+			
+		if(str.equals(word[i][1])) {
+			System.out.println("정답!");
+		}else {
+			System.out.println("틀렸습니다 정답은"+word[i][1]);
 		}
-		
-	
-	
+		}
 	} // main
 
 } // class
