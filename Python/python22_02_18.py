bf969765c0123fe42c072af9e243e12820438820
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-18 12:35:24 +0900

    python 기초 문법(1)

---
+++ Python/python22_02_18.py
@@ -0,0 +1,17 @@
+#파이썬 기초 문법
+
+#출력
+print("str")
+
+#반복문
+for i in range(3):
+    print("hello")
+
+#변수 정수값 실수값 문자열 입력 
+a=int(input()) #정수
+b=float(input()) #실수
+c=input()        #문자열
+
+#데이터 나열식 출력
+print(a,b,c)
+
