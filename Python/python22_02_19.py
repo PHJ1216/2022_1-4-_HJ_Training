66d4e362ad011df388e6061ee7e97226a17f40fc
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-19 20:27:47 +0900

    python 기초 문법(6) - 튜플

--- Python/python22_02_19.py
+++ Python/python22_02_19.py
@@ -1,39 +1,24 @@
 #파이썬 기초 문법
 
-#딕셔너리 - 값의 집합인 자료형
+#튜플 - 리스트와 유사함 (수정할 수 없고 선언 방법 빼고는 유사)
 
-#딕셔너리 기본 출력문
-dic1 = {"apple":"사과", "bird":"새", "bug":"벌레"}  #보통 중괄호를 이용한 딕셔너리 문법이 쓰임
-print(dic1)
+t1=('a','b','c',1,2,3)
+print(t1,t1[:4]) 
 
-dic2 = dict(apple = "사과", bird = "새", bug = "벌레")
-print(dic2)
+t2='a','b','c',1,2,3  #괄호 생량 가능
+print(t2,t2[:4])
 
-dic = {}
+s1=list(set([1,2,3]))
+print(s1)
 
-dic["apple"] = "사과"
-dic["fruits"] = ["바나나", "딸기", "오렌지"] #리스트도 값이 될 수 있음
-print(dic)
+t3=({'C':['c', 'c++', 'c#'],'front_end':['html','css','javascript']},{1,2,3},[1,2,3],(1,2,3),s1,3) #어떤 값도 가능
+print(t3)
 
-dic_a={'programming':['c','python','java'],'cprogrammin':['c','c#','c++']}
-dic_a["pyprogramming"]="python++, python"
-print(dic_a)
-del dic_a['programming']
-print(dic_a)
+t3[0]['edit']="anything" #중요 : 튜플 요소가 Mutable하면 수정 가능
+t3[2][2]="anything"   #리스트 크기는 변하기 않지만 대입은 가능
+print(t3)
 
+print(t1*t3[5])
+t4='a','b','c',1,1,1,2,2,3,4,4,5
+print(t4.index(1),t4.count(1))  #메서드 사용 가능
 
-#value는 어느 값이든 저장 가능
-dic_a[True] = [1, 10, 100] 
-dic_a["nums"] = {"one":{1,2,3,4}, "two":2}
-print(dic_a)
-
-
-#딕셔너리 메서드 활용
-dic_b={'int':[1,2,3,4,5],'float':[1.1,1.2,1.3,1.4,1.5],'complexer':[1+1j,2+2j,3+3j,4+4j,5+5j]}
-print(dic_b.keys()) #key값만 정렬
-print(dic_b.values()) #valie값만 정렬
-print(dic_b.items()) #튜플식으로 출력
-print(dic_b)
-print(dic_b.get("int",'x')) #해당 key가 존재 시 value값 출력 
-print(dic_b.get("intint",'x')) #key가 없을 시 'x'문자 출력
-print('int'in dic_b) #boolean형으로 출력
