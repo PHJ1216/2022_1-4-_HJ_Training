1855d9c7ec55348e91a7d09710f14da31880495d
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-22 23:55:14 +0900

    python 기초 문법(17) - 클래스 생성자와 정적 메소드

--- Python/python22_02_22.py
+++ Python/python22_02_22.py
@@ -2,62 +2,28 @@
 
 #함수 문법
 
-# 클래스-객체 기초 문법
+# 클래스 - 생성자와 정적 메소드
 
-'''class Triangle :   # 클래스 생성
-	pass             # 실행할 내용 없을 때 지나치는 키워드 pass
+class test_init :
+    cal_count=0
 
-tri1 = Triangle()    #객체 tri1~4 4개 생성  객체이름=클래스이름() 형식으로 생성
-tri2 = Triangle()
-tri3 = Triangle()
-tri4 = Triangle()'''
-
-# 클래스 변수
-
-class Triangle :   # 클래스 생성
-	height = 10     # 클래스 변수 선언
-	bottom = 4
-
-tri1 = Triangle()   # 객체 생성
-tri1.height=8
-print(tri1.height)   # 클래스 변수 접근 시 객체이름.변수 형식으로 접근
-print(tri1.bottom)
-
-tri2 = Triangle()
-print(tri2.height)  # 22 line에서 변수 값이 변했지만 클래스에 영향을 안 주므로 tri2객체에서는 height값은 10임
-print(tri2.bottom)
-
-# 인스턴스 변수와 메서드
-
-class xy_plus :
-    def plus(self, x, y) : # 메소드
-        self.x = x
-        self.y = y
-        print(x+y)
-    
-    def real_mul(self):     # 메소드 
-        return self.x*self.y
-        
-
-xy1 = xy_plus() # 객체 생성
-xy1.plus(4, 5) # 객체 메소드 실행  # self.변수이름 형식의 변수가 인스턴스 변수
-print(xy1.real_mul())  # real_mul 메소드 실행
-
-# __init__ 생성자 메소드 
-class Test_init : 
-    cal_count = 0
-    
-    def __init__(self, b, h = 5) : #생성자
+    def __init__(self, b, h) : #생성자 __init__ 수정 x 그대로 사용해야함.
         self.b = b
         self.h = h
-
+    
     def area(self) :
-        Test_init.cal_count += 1
-        
         return self.b * self.h / 2
+    
+    @staticmethod               # 정적메소드 @staticmethod로 선언 앞에 넣어줘야함 일반 함수처럼 사용o 
+    def isIsosceles(a, b) :
+        test_init.cal_count += 1
+        return a == b    
 
-test1 = Test_init(4)      #__init__메소드 적용 h는 자동으로 5
-test2 = Test_init(6, 10)
+   
+tri1 = test_init(4, 5) #호출하면서 바로 인자 전달
+tri2 = test_init(6, 10)
 
-print(test1.b, test1.h, test1.area(), test1.cal_count) # count 1
-print(test2.b, test2.h, test2.area(), test2.cal_count) # count 2
+print(tri1.b, tri1.h, tri1.area())
+print(tri2.b, tri2.h, tri2.area())
+print(tri1.isIsosceles(5,5), tri1.cal_count) # 이등변 삼각형 구별 a==b면 true 
+print(test_init.isIsosceles(5,4))           # 아니라면 false
