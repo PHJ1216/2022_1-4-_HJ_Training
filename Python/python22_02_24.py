5019adfc1b540b647980e2eaffbed911f02f687f
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-24 18:20:59 +0900

    python 기초 문법(20) - 클래스 상속 부모, 자식클래스 쉬운 예제

--- Python/python22_02_24.py
+++ Python/python22_02_24.py
@@ -1,71 +1,42 @@
 #파이썬 기초 문법
 
-# 클래스 상속 심화 
+# 클래스 상속 
 
-import math
-
-#도형
-class Shape :
-    cal_count = 0 # 클래스 변수
-    figure = "Shape" # 클래스 변수
-
-    @classmethod
-    def class_printFigure(cls) : # 클래스 메소드
-        return cls.figure
+# 부모 클래스
+class Person :
+    job = "" # 클래스 변수
     
-    @staticmethod
-    def static_printFigure() : # 정적 메소드
-        return Shape.figure
-
-#도형 상속 삼각형
-class Triangle(Shape) : # shape클래스 상속
-    figure = "triangle"
+    @classmethod # 클래스 메소드
+    def greeting(cls) :
+        print("안녕하세요 저는",cls.job,"입니다.")
     
-    def __init__(self, b, h=5) :
-        self.b = b # 인스턴스 변수
-        self.h = h # 인스턴스 변수
-         
-    def area(self) : # 메소드 추가
-        Shape.cal_count += 1
+    def printAge(self) :
+        print(self.age)
+
+class Student(Person) : # Person 상속
+    job = "student"
+    
+    def __init__(self, age) : # 생성자
+        self.age = age
         
-        return self.b * self.h / 2
+    def printAge(self) :
+        print("저의 나이는", self.age, "입니다.")    
 
-#도형 상속 정삼각형
-class EquTriangle(Triangle) : # triangle클래스 상속 겸 shape클래스도 같이 상속
-    figure = "equilateral triangle"
+class Professor(Person) : # Person 상속
+    job = "professor"   
     
-    def __init__(self, b) :
-        self.b = b
+    def __init__(self, age) :
+        self.age = age
         
-    def area(self) :
-        Shape.cal_count += 1
-        
-        return 0.25 * math.sqrt(3) * self.b ** 2
-    
-    def circumference(self) :
-        return self.b * 3
+    def printAge(self) :
+        print("저의 나이는", self.age, "입니다.") 
+   
+std = Student(14)
+pf = Professor(56)
 
-#도형 상속 원
-class Circle(Shape) : # shape클래스 상속 
-    figure = "circle"
-    
-    def __init__(self, r) :
-        self.r = r
-        
-    def area(self) :
-        Shape.cal_count += 1
-        
-        return math.pi * self.r ** 2
-    
-    def circumference(self) :
-        return 2 * math.pi * self.r
 
-tri = Triangle(10, 4)
-eqtri = EquTriangle(3)
-cir = Circle(8)
+std.greeting()
+std.printAge()
 
-print(cir.static_printFigure(), cir.area(), cir.circumference())
-print(tri.class_printFigure(), tri.area(), cir.circumference())
-print(eqtri.class_printFigure(), eqtri.area(), cir.circumference())
-
-print(Shape.cal_count, cir.cal_count, tri.cal_count) #  3, 3, 3
+pf.greeting()
+pf.printAge()
