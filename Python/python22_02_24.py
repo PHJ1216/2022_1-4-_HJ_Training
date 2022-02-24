f10af8817a31ffa4dddc7a895d9205a5e77e92b8
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-24 22:24:34 +0900

    python 기초 문법(21) - 클래스 다중 상속

--- Python/python22_02_24.py
+++ Python/python22_02_24.py
@@ -1,42 +1,29 @@
 #파이썬 기초 문법
 
-# 클래스 상속 
+# 클래스 다중 상속
 
-# 부모 클래스
-class Person :
-    job = "" # 클래스 변수
+class First :
+    name = "first1"
+    def __init__(self) :
+        print("First class1")
     
-    @classmethod # 클래스 메소드
-    def greeting(cls) :
-        print("안녕하세요 저는",cls.job,"입니다.")
-    
-    def printAge(self) :
-        print(self.age)
-
-class Student(Person) : # Person 상속
-    job = "student"
-    
-    def __init__(self, age) : # 생성자
-        self.age = age
+    def printFirst(self) :
+        print("first1")
         
-    def printAge(self) :
-        print("저의 나이는", self.age, "입니다.")    
-
-class Professor(Person) : # Person 상속
-    job = "professor"   
+class Second :
+    name = "second2"
+    def __init__(self) :
+        print("First class2")
     
-    def __init__(self, age) :
-        self.age = age
-        
-    def printAge(self) :
-        print("저의 나이는", self.age, "입니다.") 
-   
-std = Student(14)
-pf = Professor(56)
+    @classmethod
+    def printName(cls) :
+        print(cls.name)
 
+#상속해야 할 부모클래스가 두 개인 경우 충돌 가능
+#파이썬은 MRO에 따라 다중 상속을 진행
+class Third(First, Second) :
+    pass
 
-std.greeting()
-std.printAge()
-
-pf.greeting()
-pf.printAge()
+third = Third()
+third.printName()
+third.printFirst()
