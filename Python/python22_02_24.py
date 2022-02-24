d00abf1d2c4752b0b0c80f379cee83091c91f71a
PHJ1216 <98177843+PHJ1216@users.noreply.github.com>
2022-02-24 23:33:57 +0900

    python 기초 문법(23) - 예외 처리 try문 활용

--- Python/python22_02_24.py
+++ Python/python22_02_24.py
@@ -1,31 +1,41 @@
 #파이썬 기초 문법
 
-# 모듈 
-# 1. 효율성 1-2. 용도에 따라 파일로 구분 1-3. 다른 파일에서 필요할 때 가져오기 
-# 2. 타인의 코드를 나의 코드에 활용 가능 
+# 예외처리 
 
-import random
+# try~except
+try:
+  10 / 0
+except ZeroDivisionError as e:
+  print(e)
 
-list = ['a', 'b', 'c', 'd']
-a = random.random()
-b = random.randrange(1, 10)
-c = random.choice(list)
-print(a,b,c)
+# try~else
+try:
+  10 / 2
+except ZeroDivisionError as e:
+  print(e)
+else:
+  print("Success!")
 
-"""모듈을 만들 때 우선 기능이 작동하는 실직적 코드를 메인코드와 같은 경로에 저장하고 import로 선언"""
+# try~finally
+try:
+  10 / 2
+except ZeroDivisionError as e:
+  print(e)
+else:
+  print("Success!")
+finally:
+	print("ZeroDivisionError Check") # try문이 종결되면 무조건 finally 내의 코드를 실행함
 
-#file 이름 : my_module.py 
-def three_times(a): return a * 3  #기능 파일
-def ten_times(a): return a * 10 
-"""-------------------------------------------"""
-import my_module                    #메인 파일
-print(my_module.three_times(10)) #=> 30 
-print(my_module.ten_times(10)) #=> 100
-"""-------------------------------------------"""
-from my_module import three_times
-from my_module import ten_times  #from 모듈이름 import 모듈함수 형식으로 쓰면
+"""------------------------"""
+# 오류 회피 방법
+try:
+  10 / 0
+except ZeroDivisionError:
+  pass
 
-print(three_times(10)) #=> 30    #기능을 쓸 때 편함
-print(ten_times(10)) #=> 100
-
-"""추가로 from 모듈이름 improt*  별을 표현하면 모든 함수 사용가능"""
+"""------------------------"""
+# 오류 발생 방법
+try:
+  raise NameError   # 고의로 오류 발생시킴
+except NameError:
+  print("NameError occurred")
